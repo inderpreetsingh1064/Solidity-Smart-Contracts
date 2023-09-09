cf5c4625e161c0036e686d82992251176f51b6f7
Inderpreet Singh <Inderpreetsingh1064@gmail.com>
2023-09-10 00:07:14 +0530

    v3 mint functionality added

--- uniswapv3/src/clamm.sol
+++ uniswapv3/src/clamm.sol
@@ -1,15 +1,47 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.19;
+import "./interface/IERC20.sol";
 
+import "./lib/TickMath.sol";
 import "./lib/Tick.sol";
+import "./lib/Position.sol";
+import "./lib/SafeCast.sol";
 
-contract Counter {
+struct Slot0 {
+    // 160 / 8 = 20 bytes
+    uint160 sqrtPriceX96;
+    // 24 / 8 = 3 bytes
+    int24 tick;
+    // 1 byte
+    bool unlocked;
+}
+
+function checkTicks(int24 tickLower, int24 tickUpper) pure {
+    require(tickLower < tickUpper);
+    require(TickMath.MIN_TICK <= tickLower);
+    require(tickUpper <= TickMath.MAX_TICK);
+}
+
+contract CLAMM {
+    using SafeCast for int256;
+    using Position for mapping(bytes32 => Position.Info);
+    using Position for Position.Info;
+
     address public immutable token0;
     address public immutable token1;
     uint24 public immutable fee;
     int24 public immutable tickSpacing;
     uint128 public immutable maxLiquidityPerTick;
 
+    Slot0 public slot0;
+    mapping(bytes32 => Position.Info) public positions;
+
+    modifier lock() {
+        require(slot0.unlocked, "locked");
+        slot0.unlocked = false;
+        _;
+        slot0.unlocked = true;
+    }
     constructor(
         address _token0,
         address _token1,
@@ -22,4 +54,78 @@ contract Counter {
         tickSpacing = _tickSpacing;
         maxLiquidityPerTick = Tick.tickSpacingToMaxLiquidityPerTick(_tickSpacing);
     }
+
+    function initialize(uint160 sqrtPriceX96) external {
+        require(slot0.sqrtPriceX96 == 0, "Already Intitialized");
+        int24 tick = TickMath.getTickAtSqrtRatio(sqrtPriceX96);
+        slot0 = Slot0({sqrtPriceX96: sqrtPriceX96, tick: tick, unlocked: true});
+    }
+
+    function _updatePosition(
+        address owner,
+        int24 tickLower,
+        int24 tickUpper,
+        int128 liquidityDelta,
+        int24 tick
+    ) private returns (Position.Info storage position) {
+        position = positions.get(owner, tickLower, tickUpper);
+
+        uint256 _feeGrowthGlobal0X128 = 0;
+        uint256 _feeGrowthGlobal1X128 = 0;
+
+        position.update(
+            liquidityDelta, 0, 0
+        );    
+    }
+
+    struct ModifyPositionParams {
+        address owner;
+        int24 tickLower;
+        int24 tickUpper;
+        int128 liquidityDelta;
+    }
+
+    function _modifyPosition(ModifyPositionParams memory params)
+        private
+        returns (Position.Info storage position, int256 amount0, int256 amount1)
+    {
+        checkTicks(params.tickLower, params.tickUpper);
+        Slot0 memory _slot0 = slot0;
+
+        _updatePosition(
+            params.owner,
+            params.tickUpper,
+            params.tickLower,
+            params.liquidityDelta,
+            _slot0.tick
+        );
+
+        return (positions[bytes32(0)], 0, 0);
+    }
+
+    function mint(
+        address recipient,
+        int24 tickLower,
+        int24 tickUpper,
+        uint128 amount
+    ) external lock returns (uint256 amount0, uint256 amount1) {
+        require(amount > 0, "amount 0");
+        (, int256 amount0Int, int256 amount1Int) = _modifyPosition(
+            ModifyPositionParams({
+                owner: recipient,
+                tickLower: tickLower,
+                tickUpper: tickUpper,
+                liquidityDelta: int256(uint256(amount)).toInt128()
+            })
+        );
+        amount0 = uint256(amount0Int);
+        amount1 = uint256(amount1Int);
+        if (amount0 > 0) {
+            IERC20(token0).transferFrom(msg.sender, address(this), amount0);
+        }
+        if (amount1 > 0) {
+            IERC20(token1).transferFrom(msg.sender, address(this), amount1);
+        }
+    }
+
 }
